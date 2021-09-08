d879fb91c8ae2de789b233f776c613540c43ab73
Fabio.Masao <f.masaoishizu@gmail.com>
2021-09-08 16:24:51 -0300

    paginação

--- backend/src/main/java/com/sds/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/sds/dsvendas/entities/Sale.java
@@ -27,6 +27,8 @@ public class Sale {
 	@JoinColumn(name = "seller_id")
 	private Seller seller;	
 	
+	public Sale() {
+	}
 	
 	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
 		this.id = id;
