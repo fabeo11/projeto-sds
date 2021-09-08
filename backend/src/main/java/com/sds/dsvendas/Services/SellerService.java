b8c5142c1cfec5048599f52c38ebdabe4c71f93c
Fabio.Masao <f.masaoishizu@gmail.com>
2021-09-08 15:45:39 -0300

    camadas

---
+++ backend/src/main/java/com/sds/dsvendas/Services/SellerService.java
@@ -0,0 +1,25 @@
+package com.sds.dsvendas.Services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sds.dsvendas.dto.SellerDTO;
+import com.sds.dsvendas.entities.Seller;
+import com.sds.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result =  repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+	
+}
